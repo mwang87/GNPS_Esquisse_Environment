00151818aa2558033dee2c788408716daded5a36
Mingxun Wang <mwang87@gmail.com>
2020-03-16 22:48:43 -0700

    making way more succinct

--- file_upload/esquisse.R
+++ file_upload/esquisse.R
@@ -10,73 +10,16 @@ args = commandArgs(trailingOnly=TRUE)
 font_import()
 loadfonts()
 
+### Whole Shiny app ###
 ui <- fluidPage(
-  
-  titlePanel("Upload Esquisse Data Exploration Interface"),
-  
-  sidebarLayout(
-    sidebarPanel(
-        #textInput(inputId="uploadfile", "File Name (Do not touch)", value = "", width = NULL, placeholder = NULL),
-        # Input: Select a file ----
-        fileInput(inputId="file1", "Choose CSV File",
-                  multiple = FALSE,
-                  accept = c("text/csv",
-                          "text/comma-separated-values,text/plain",
-                          ".csv"))
-    ),
-    mainPanel(
-      tabsetPanel(
-        tabPanel(
-          title = "esquisse",
-          esquisserUI(
-            id = "esquisse", 
-            header = FALSE, # dont display gadget title
-            choose_data = FALSE # dont display button to change data
-          )
-        ),
-        tabPanel(
-          title = "output",
-          verbatimTextOutput("module_out")
-        )
-      )
+    esquisserUI(
+        id = "esquisse", 
+        container = esquisseContainer(fixed = TRUE)
     )
-  )
 )
 
 server <- function(input, output, session) {
-
-  # observe({
-  #   query <- parseQueryString(session$clientData$url_search)
-  #   if (!is.null(query[['filename']])) {
-  #     updateTextInput(session, "uploadfile", value=query[['filename']])
-  #   }
-  # })
-
-  #TODO: refactor this portion of the code because, we don't fully understand global
-  data <- reactiveValues(data=data.frame(), name="upload")
-
-  # observeEvent(input$uploadfile, {
-  #   if (nchar(input$uploadfile) > 2){
-  #     data$data <- fread(sprintf("/uploads/%s", basename(input$uploadfile)))
-  #   }
-  # })
-
-  observeEvent(input$file1, {
-    print(input$file1)
-    data$data = read.csv(file=input$file1$datapath)
-  })
-
-  
-  result <- callModule(
-    module = esquisserServer,
-    id = "esquisse",
-    data = data
-  )
-  
-  output$module_out <- renderPrint({
-    str(reactiveValuesToList(result))
-  })
-  
+    callModule(module = esquisserServer, id = "esquisse", dataModule="ImportFile")
 }
 
 shinyApp(ui, server)
